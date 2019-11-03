639ce844b59963880393889120c12d1112e3d6eb
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-11-03 19:03:18 +0000

    kaggle

---
+++ src/ensemble/ensemble5.py
@@ -0,0 +1,47 @@
+import os
+import pandas as pd
+os.environ['CUDA_VISIBLE_DEVICES']='0'
+
+def run_ensemble():
+
+    file=[
+    
+        '../input/5resnext50-4efficientnetb3-3inceptionv4/model001_fold0_ep2_test_tta5.csv',
+        '../input/5resnext50-4efficientnetb3-3inceptionv4/model001_fold1_ep2_test_tta5.csv',
+        '../input/5resnext50-4efficientnetb3-3inceptionv4/model001_fold2_ep2_test_tta5.csv',
+        '../input/5resnext50-4efficientnetb3-3inceptionv4/model001_fold3_ep2_test_tta5.csv',
+        '../input/5resnext50-4efficientnetb3-3inceptionv4/model001_fold4_ep2_test_tta5.csv',
+        '../input/5resnext50-4efficientnetb3-3inceptionv4/model003_2_fold3_ep2_test_tta5.csv',
+        '../input/5resnext50-4efficientnetb3-3inceptionv4/model003_fold0_ep1_test_tta5.csv',
+        '../input/5resnext50-4efficientnetb3-3inceptionv4/model003_fold2_ep2_test_tta5.csv',
+        '../input/5resnext50-4efficientnetb3-3inceptionv4/model003_fold4_ep2_test_tta5.csv',
+        '../input/5resnext50-4efficientnetb3-3inceptionv4/model004_fold0_ep2_test_tta5.csv',
+        '../input/5resnext50-4efficientnetb3-3inceptionv4/model004_fold1_ep2_test_tta5.csv',
+        '../input/5resnext50-4efficientnetb3-3inceptionv4/model004_fold2_ep2_test_tta5.csv',
+        
+        # add retrain files here
+
+    ]
+
+
+    ############################################################
+
+
+
+
+    csv_file='all.csv'
+    label = 0
+    for f in file:
+        #print(f)
+        df = pd.read_csv(f)
+        df = df.sort_values('ID')
+
+        id = df['ID'].values
+        label += df['Label'].values
+
+    label = label/len(file)
+    df = pd.DataFrame(zip(id, label), columns=['ID', 'Label'])
+    df.to_csv(csv_file, index=False)
+    return df
+
+df=run_ensemble()
